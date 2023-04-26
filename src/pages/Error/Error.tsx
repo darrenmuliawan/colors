becd9ff8f140c4c289487dc5365ef89dc3e0668b
darrenmuliawan <darrenm2@illinois.edu>
2023-04-26 13:17:09 +0700

    feat: add error page

---
+++ src/pages/Error/Error.tsx
@@ -0,0 +1,8 @@
+export const ErrorPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-5xl">DANG!</h1>
+      <h2 className="text-3xl">YOU SHOULD NOT SEE THIS! WILL FIX!</h2>
+    </div>
+  );
+};
