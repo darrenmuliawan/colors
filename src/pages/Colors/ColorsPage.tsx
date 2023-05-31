2129c095bd3931fbb0f256fc9254199136078ad1
darrenmuliawan <darrenm2@illinois.edu>
2023-05-31 22:07:10 +0700

    feat: add mobile colors page

--- src/pages/Colors/ColorsPage.tsx
+++ src/pages/Colors/ColorsPage.tsx
@@ -6,12 +6,12 @@ import { WallpaperEditor } from './WallpaperEditor';
 export const ColorsPage = () => {
   return (
     <Container>
-      <section className="relative h-[66vh] w-full flex flex-col items-center justify-evenly py-[100px]">
+      <section className="relative h-[66vh] w-full flex flex-col items-center justify-evenly pt-[100px] lg:pb-[50px] pb-[25px]">
         <div className="flex items-center justify-center w-full h-full">
-          <h1 className="text-5xl font-bold text-center">Colors</h1>
+          <h1 className="text-4xl lg:text-5xl font-bold text-center">Colors</h1>
         </div>
         <div className="w-full flex items-center justify-center flex-col h-full">
-          <h2 className="text-3xl w-[66%] text-center">
+          <h2 className="text-xl lg:text-3xl w-[66%] text-center">
             Dive into a collection of 100+ curated colors, hand-picked to elevate your digital
             experience. Customize your phone or computer wallpaper with your favorite hue from our
             kaleidoscopic array of colors and add some personality to your devices.
